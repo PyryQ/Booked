21aba56b94c5b4ce5a89bd66a225dfadc1ad367c
Pyry <pyry@ogoship.com>
2022-10-22 22:53:17 +0300

    Some rearangements and comments to sqlcontroller

--- Booked/Controllers/SQLController.cs
+++ Booked/Controllers/SQLController.cs
@@ -19,57 +19,7 @@ namespace Booked.Controllers
             return ConfigurationManager.ConnectionStrings[id].ConnectionString;
         }
 
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="bookId"></param>
-        /// <returns></returns>
-        public static int PostNewDBBook(BookPostInfo book)
-        {
-            try
-            {
-                using (SQLiteConnection con = new SQLiteConnection(LoadConnectionString()))
-                {
-                    con.Execute("INSERT INTO books (title, author, year, publisher, description) VALUES (@Title, @Author, @Year, @Publisher, @Description)", book);
-
-                    string sql = @"SELECT MAX(id) FROM books";
-                    long lastId = (long)con.ExecuteScalar(sql);
-
-                    return (int)lastId;
-                }
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
-        }
-
         #region GET
-
-        /// <summary>
-        /// Returns all the books in the database
-        /// </summary>
-        /// <param name="author"></param>
-        /// <param name="publisher"></param>
-        /// <param name="year"></param>
-        /// <returns></returns>
-        /// <exception cref="Exception"></exception>
-        public static List<Book> GetDBBooks()
-        {
-            try
-            {
-                using (IDbConnection con = new SQLiteConnection(LoadConnectionString()))
-                {
-                    var output = con.Query<Book>("SELECT * from books", new DynamicParameters());
-
-                    return output.ToList();
-                }
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
-        }
 
         /// <summary>
         /// Returns book by id
@@ -97,7 +47,7 @@ namespace Booked.Controllers
         }
 
         /// <summary>
-        /// 
+        /// Returns all the books from the database
         /// </summary>
         /// <returns></returns>
         public static List<Book> GetAllDBBooks()
@@ -118,6 +68,31 @@ namespace Booked.Controllers
         }
 
         #endregion GET
+
+        /// <summary>
+        /// Post a new book into the database. Returns the latest id.
+        /// </summary>
+        /// <param name="bookId"></param>
+        /// <returns></returns>
+        public static int PostNewDBBook(BookPostInfo book)
+        {
+            try
+            {
+                using (SQLiteConnection con = new SQLiteConnection(LoadConnectionString()))
+                {
+                    con.Execute("INSERT INTO books (title, author, year, publisher, description) VALUES (@Title, @Author, @Year, @Publisher, @Description)", book);
+
+                    string sql = @"SELECT MAX(id) FROM books";
+                    long lastId = (long)con.ExecuteScalar(sql);
+
+                    return (int)lastId;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
 
         /// <summary>
         /// Delete book based on given id
