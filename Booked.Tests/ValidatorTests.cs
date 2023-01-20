b6af095779e6af8fa9f53281fee56e4961b5609d
Pyry Qvick <pyry.qvick@buutti.com>
2023-01-20 14:56:51 +0200

    Starting possible books problem testing

--- Booked.Tests/ValidatorTests.cs
+++ Booked.Tests/ValidatorTests.cs
@@ -3,6 +3,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Booked;
+using Booked.Models.Classes;
+using Booked.Models.Interfaces;
 using global::Booked.SupportClasses;
 using Xunit;
 
@@ -59,12 +61,43 @@ namespace Booked.Tests
             string expected = "Year needs to be an integer.";
 
             // Act
-            string actual = BookValidator.PossibleQueryProblems("Author", (decimal)2000.02, "Publisher");
+            string actual = BookValidator.PossibleQueryProblems(author, year, publisher);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void PossibleQueryProblems_AllFiltersIncorrect_ReturnsErrorString()
+        {
+            // Arrange
+            string expected = "Author field is empty. Publisher field is empty. Year needs to be an integer.";
+
+            // Act
+            string actual = BookValidator.PossibleQueryProblems("", (decimal)2000.02, "");
 
             // Assert
             Assert.Equal(expected, actual);
         }
 
         #endregion PossibleQueryProblems
+
+        #region PossibleBookProblems
+
+        //[Fact]
+        //public void PossibleBookProblems_AllInfoGiven_ReturnsEmptyString()
+        //{
+        //    // Arrange
+        //    string expected = string.Empty;
+
+        //    // Act
+        //    Book book = new Book() { Title ="Title", Author = "Author", };
+
+        //    string actual = BookValidator.PossibleQueryProblems("", (decimal)2000.02, "");
+
+        //    // Assert
+        //}
+
+        #endregion PossibleBookProblems
     }
 }
